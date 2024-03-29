39ce954d2954b9440f45bbb0c65f2d47b10f0db9
Warhorst <46809804+Warhorst@users.noreply.github.com>
2024-03-29 20:51:30 +0100

    added month and date types

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// Represents an International Fixed Calendar date
+pub struct IFC {
+    pub day: u8
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    Sol,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub enum WeekDay {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+    /// the 29th of June, which is only present in leap years
+    LeapDay,
+    /// the 29th of December, which is the last day of the year
+    YearDay
 }
